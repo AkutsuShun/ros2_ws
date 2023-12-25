0faa5ce2ecbf5c85c91469a714dc3b60ba40b2df
AkutsuShun <shumy06202000@gmail.com>
2023-12-26 01:44:11 +0900

    check_taker_&_add

--- install/mypkg/lib/python3.8/site-packages/mypkg/talker.py
+++ install/mypkg/lib/python3.8/site-packages/mypkg/talker.py
@@ -1,18 +1,29 @@
-import rclpy                     #ROS 2のクライアントのためのライブラリ
-from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
-from std_msgs.msg import Int16   #通信の型（16ビットの符号付き整数）
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
+
+class Talker():
+    def __init__(self, node):
+         self.pub = node.create_publisher(Int16, "countup", 10)
+         self.n = 0
+         node.create_timer(0.5, self.cb) #selfをつける。
+
+    def cb(self):      #インデントをあげてselfを引数に
+         msg = Int16()
+         msg.data = self.n     #talker -> self
+         self.pub.publish(msg) #talker -> self
+         self.n += 1           #talker -> self
+
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin(node)
+
+if __name__ == '__main__':
+    main()
 
 rclpy.init()
-node = Node("talker")            #ノード作成（nodeという「オブジェクト」を作成）
-pub = node.create_publisher(Int16, "countup", 10)   #パブリッシャのオブジェクト作成
-n = 0 #カウント用変数
-
-def cb():          #17行目で定期実行されるコールバック関数
-     global n       #関数を抜けてもnがリセットされないようにしている
-     msg = Int16()  #メッセージの「オブジェクト」
-     msg.data = n   #msgオブジェクトの持つdataにnを結び付け
-     pub.publish(msg)        #pubの持つpublishでメッセージ送信
-     n += 1
-
-node.create_timer(0.5, cb)  #タイマー設定
-rclpy.spin(node)            #実行（無限ループ）
+node = Node("talker")
+talker = Talker(node) #この一行でパブリッシャが動き出す。
+rclpy.spin(node)
